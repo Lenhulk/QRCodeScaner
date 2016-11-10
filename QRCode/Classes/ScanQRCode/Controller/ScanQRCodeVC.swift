ec3dade6f16c3701f2f9855c20483a510b28a837
Lenhulk <Lenhulk@yeah.net>
2016-11-10 15:21:29 +0800

    初始化，搭建基本框架，重构storyboard

---
+++ QRCode/Classes/ScanQRCode/Controller/ScanQRCodeVC.swift
@@ -0,0 +1,21 @@
+//
+//  ScanQRCodeVC.swift
+//  QRCode
+//
+//  Created by Lenhulk on 2016/11/10.
+//  Copyright © 2016年 Lenhulk. All rights reserved.
+//
+
+import UIKit
+
+class ScanQRCodeVC: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
